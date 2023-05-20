1e9c156a9e9100c7caf14d0c782ed3c6da7868c2
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-05-20 02:01:15 -0600

    :sparkles: GPT API

---
+++ controllers/chatgpt.controller.js
@@ -0,0 +1,33 @@
+const { response, request } = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+const callChatGpt = async (req = request, res = response) => {
+  try {
+    const { prompt } = req.body;
+
+    let queryObj = {
+      model: "gpt-3.5-turbo",
+      messages: [{ "role": "user", "content": prompt }],
+      max_tokens: 200,
+      temperature: 1
+    }
+
+    const completion = await openai.createChatCompletion(queryObj);
+    const message = completion.data.choices[0].message;
+    const usage = completion.data.usage;
+
+    res.json({ message, usage });
+  } catch (error) {
+    console.log(JSON.stringify(error));
+  }
+}
+
+module.exports = {
+  callChatGpt
+}
